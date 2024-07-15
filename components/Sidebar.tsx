e09e627b86af1f0c6b8b2f1b0635ca78365cbff0
treyjaded <trevonjcharles@gmail.com>
2024-07-15 17:09:25 -0400

    Adding Song Library Div to Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,6 +2,11 @@
 
 import { usePathname } from "next/navigation";
 import { useMemo } from "react";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import Box from "./Box";
+
+
 
 interface SidebarProps {
   children: React.ReactNode;
@@ -15,21 +20,40 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
 
   const routes = useMemo(() => [
     {
+      icon: HiHome,
       label: 'Home',
       active: pathname !== '/search',
       href: '/',
     },
     {
+      icon: BiSearch,
       label: 'Search',
       active: pathname === '/search',
       href: '/search',
     }
-
   ], [pathname]);
 
   return (
-    <div>
-      {children}
+    <div className="flex h-full">
+      <div
+        className="
+        hidden 
+        md:flex
+        flex-col 
+        gap-y-2 
+      bg-black 
+        h-full 
+        w-[300px]
+        p-2
+        "
+      >
+        <Box>
+          Sidebar Navigation
+        </Box>
+        <Box className="overflow-y-auto h-full">
+          Song Library
+        </Box>
+      </div>
     </div>
   );
 }
