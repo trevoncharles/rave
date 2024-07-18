7a4bf2b3459762d37e84f6a725dd531fcf5a84cc
treyjaded <trevonjcharles@gmail.com>
2024-07-18 00:13:31 -0400

    Added Button to Home

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,3 +1,4 @@
+import ListItem from "@/components/ListItem"
 import Header from "@/components/header"
 
 export default function Home() {
@@ -11,7 +12,30 @@ export default function Home() {
     overflow-y-hidden
     "> 
     <Header>
-    HEADER
+      <div className="
+      mb-2">
+        <h1 className="
+        text-white
+        text-3xl
+        font-semibold">
+          Welcome Back
+        </h1>
+    <div className="
+    grid
+    grid-cols-1
+    sm:grid-col-2
+    xl:grid-cols-3
+    2xl:grid-col-4
+    gap-3
+    mt-4">
+
+      <ListItem 
+      image = "/images/logo-white.png" 
+      name = "Liked Songs"
+      />
+      
+    </div>
+      </div>
     </Header>
     </div>
   );
